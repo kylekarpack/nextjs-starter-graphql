ae49a60b3464dd10226421c12a2629766a534c9f
Kyle Karpack <karpack@adobe.com>
2021-11-11 10:13:41 -0800

    Add widget display tests

---
+++ components/WidgetDisplay.test.tsx
@@ -0,0 +1,50 @@
+import { MockedProvider, MockedResponse } from "@apollo/client/testing";
+import { act, render } from "@testing-library/react";
+import { GraphQLError } from "graphql";
+import React from "react";
+import { GET_WIDGETS } from "utilities/queries";
+import { Widget } from "utilities/types";
+import WidgetDisplay from "./WidgetDisplay";
+
+const emptyMock: MockedResponse<Record<string, Widget[]>> = {
+  request: {
+    query: GET_WIDGETS
+  },
+  result: {
+    data: {
+      data: []
+    }
+  }
+};
+
+describe("widget display component", () => {
+  it("renders", () => {
+    const mocks = [emptyMock];
+
+    const cmp = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <WidgetDisplay />
+      </MockedProvider>
+    );
+
+    expect(cmp.container).toBeVisible();
+  });
+
+  it("renders error", async () => {
+    const mocks = [
+      {
+        ...emptyMock,
+        error: new GraphQLError("Bad")
+      }
+    ];
+
+    const cmp = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <WidgetDisplay />
+      </MockedProvider>
+    );
+
+    await act(() => new Promise((resolve) => setTimeout(resolve, 0)));
+    expect(cmp.getByTestId("error")).toHaveTextContent("Error");
+  });
+});
